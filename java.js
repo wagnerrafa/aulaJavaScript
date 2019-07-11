b5f18605be57f8a75aa8262dfe8ff0b1d16e0398
wagnerrafa <wagner_rafa@hotmail.com.br>
2019-07-11 11:54:17 -0300

    aula java

---
+++ java.js
@@ -0,0 +1,86 @@
+// console.log('Hello world');
+
+// let nomeDoUsuario = 'Wagner';
+// let idade=25;
+// let vivo=true;
+// console.log(nomeDoUsuario, idade, vivo);
+// prompt('qual é seu nome?');
+// let nomeDoUsuario;
+// let programador;
+// nomeDoUsuario = prompt('qual é seu nome?');
+// alert('Ola ' + nomeDoUsuario);
+// programador=confirm('voce já programa?');
+
+// if(programador==true){
+//     alert('parabens');
+//     programador=confirm('já programa em c++?');}
+//     if(programador==true){
+//         alert('PARABENS');
+    
+// }
+//         else{
+//     alert('que pena');
+// }
+// let nomeDoAluno;
+// let materia;
+// let nota;
+
+// nomeDoAluno=prompt('Qual é seu nome?');
+// materia=prompt('Qual a materia?');
+// nota=prompt("qual foi a nota?");
+
+//  if(nota>10 || nota<0){
+//     alert('Nota invalida')
+// }
+// else if(nota>=5){
+//     alert('Parabens, o aluno '+ nomeDoAluno +' foi aprovado na materia '+ materia)}
+//     else if(nota>=4.5){
+//         alert('Parabens, o aluno '+ nomeDoAluno +' foi aprovado na materia '+ materia+ ' pelo sistema');
+//     }
+//     else{
+//         alert('Parece que a nota do aluno '+nomeDoAluno +" na materia "+materia+" foi insuficiente");
+//     }
+
+//     vetor =[nome=prompt('Nome'),
+//     idade=prompt('idade'),
+//     materia=prompt('materia')
+// ];
+
+// alert(nome + idade + materia);
+
+// let wagner = {
+//     nome: 'wagner',
+//     sobrenome: 'oliveira',
+//     idade: 25,
+//     cursos: {
+//         ingles: 'basico',
+//         espanhol:'intermediario',
+//         informatica: 'avançado',}
+// };
+// b=1;
+// a=1;
+// for(i=0;i<10;i++){
+//     alert(i);
+//     a=b+1;
+//     b++;
+// }
+// alert(a);
+// resposta='sim';
+// media=0;
+// while(resposta=='sim'){
+//     nota=prompt('nota');
+//     media=nota+media;
+//     resposta=prompt('Continuar');
+
+// }
+// alert(media/2);
+
+
+
+let escada=Number(prompt('Quantos lances de escada?'));
+let material=prompt('Qual o material?');
+let i=material;
+
+for(ij=0;ij<escada;ij++){
+console.log(material=material+i);
+}
