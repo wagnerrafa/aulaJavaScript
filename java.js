a6971f32a6ff6e921eeb2148b4709f3bbf239efa
wagnerrafa <wagner_rafa@hotmail.com.br>
2019-07-12 11:49:26 -0300

    aula function

--- java.js
+++ java.js
@@ -77,10 +77,129 @@
 
 
 
-let escada=Number(prompt('Quantos lances de escada?'));
-let material=prompt('Qual o material?');
-let i=material;
+// let escada=Number(prompt('Quantos lances de escada?'));
+// let material=prompt('Qual o material?');
+// let i=material;
 
-for(ij=0;ij<escada;ij++){
-console.log(material=material+i);
+// for(ij=0;ij<escada;ij++){
+// console.log(material=material+i);
+// }
+
+
+// Aula02
+
+
+// let nome = 'rafa';
+// let numeros =(
+//     [1,2,3,4,5],
+//     [6,7,8,9]
+// );
+
+// console.log(numeros[1][2]);
+
+// let pessoa = {
+//     nome : 'rafael',
+//     idade: 25,
+//     cpf: '123.456.78-90'
+// };
+
+// let nome1 =prompt('Seu nome');
+// let idade = prompt('Sua idade');
+
+// let nome;
+// let pessoa = [];
+// let i;
+// for(i=0;i<3;i++)
+// {
+
+// pessoa[i] = nome()}
+
+
+// let frase = idade >= 30 ? 'seu velho' : 'novinho';
+
+
+// for(i=2;i<=100;i++)
+// {
+//     console.log(i)
+//     i++;
+// }
+
+// for(i=2;i<=100;i++)
+// {
+//      if(i%2==0)
+//     { console.log(i)};
+// }
+
+// let nomes=[
+//     'rafael', 'ana', 'leo', 'gabriel'];
+
+// for(i=0;i<=3;i++)
+// {
+//     console.log(nomes[i]);
+// }
+
+// for(let nome of nomes)
+// {
+//     console.log(nome);
+// }
+
+// for(let indice in nomes)
+// {
+//     console.log(indice);    
+// }
+
+
+// Funções
+
+
+// function dizerOi()
+// {
+//     alert('oi');
+//     alert("seja bem vindo");
+// }
+// let dizerOi=function ()
+// {
+//     alert('oi');
+//     alert("seja bem vindo");
+// }
+// dizerOi();
+
+// let dizerOi = () => {
+//     alert('oi');
+//     alert("seja bem vindo");
+// }
+// dizerOi();
+
+// function teste()
+// {
+//     let numero=0;
+    
+// }
+// console.log(numero);
+
+// let numero1=10;
+// let numero2=20;
+
+// function escolherMaior(numero1, numero2)
+// {
+//     return maior = numero1>numero2 ? numero1 : numero2;
+// }
+
+
+let nomes=[
+    'rafael', 'ana', 'leo', 'gabriel'];
+let palavra=prompt('nome');
+
+function buscarPalavra()
+{
+    for(i=0;i<3;i++)
+    {
+        if(nomes[i]==palavra){
+       return true;
+    } 
+} return false;
 }
+// if(return==true){
+//     alert('sim');
+// }
+// else alert('nao');   
